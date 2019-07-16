d774003b90f619e893c0ac94685aed3a2176ddad
Piers Chamberlain <piers.chamberlain@assurity.co.nz>
2019-07-16 14:57:49 +1200

    Restored port logic

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const config = require('./config.json');
+const port_config = require('./config.js');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
@@ -12,20 +13,20 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.set('port',config.port);
+app.set('port',port_config.port);
 
 app.use('/', express.static('public'))
 app.use('/', index);
 app.use('/who', who);
 app.use('/contact', contact);
 
-app.listen(config.port, () => {
-  console.log(`Demo app is running on ${config.port}!`);
+app.listen(port_config.port, () => {
+  console.log(`Demo app is running on ${port_config.port}!`);
 });
 
 const Rox = require("rox-node");
 global.flags = {
-  style_brown: new Rox.Flag()
+  style_brown: new Rox.Flag(true)
 };
 Rox.register('node_demo', flags);
 Rox.setup(config.FlagKey)
