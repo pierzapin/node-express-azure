e357c863af072c5d7f74da598f0cf9e3884f516d
Piers Chamberlain <piers.chamberlain@assurity.co.nz>
2019-07-17 10:43:50 +1200

    Disappear the Banner for Prod environments

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
+bannerhtml = '';
 
 /* GET home page. */
 router.get('/', (req, res) => {
 
+  if (global.env_name.toLowerCase().indexOf('prod') !== 0){
+    bannerhtml = '<section class="banner">'+global.env_name+'</section>'
+  }
+
   if (global.flags.style_brown.isEnabled()) {    
     res.render('index', {
-      title: 'Welcome to Contoso Unlimited!',  stylesheet: 'assets/css/main_alternative.css', bannertext: global.env_name
+      title: 'Welcome to Contoso Unlimited!',  
+      stylesheet: 'assets/css/main_alternative.css', 
+      banner: bannerhtml
     });
   }
   else {
